ead0dfe5b8adac05bd565a9336e261bb41e839bc
Andrea Ciliberti <meziu210@icloud.com>
2023-02-15 13:58:51 +0100

    init function removal

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,3 @@ mod mutex;
 mod rwlock;
 mod thread;
 mod thread_keys;
-
-/// Reference this function somewhere (eg. ´use pthread_3ds::init´ in the main crate) to import all pthread implementations.
-#[inline(always)]
-pub fn init() {}
